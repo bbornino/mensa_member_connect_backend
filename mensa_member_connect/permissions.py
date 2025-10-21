bed01764e996b17b77b46e470f9bcde905e5216f
Billie Lee <billielee@gmail.com>
2025-10-21 12:26:03 -0400

    Add IsAdminRole

---
+++ mensa_member_connect/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdminRole(BasePermission):
+    """
+    Custom permission to check if user has admin role.
+    """
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.role == 'admin')
+
